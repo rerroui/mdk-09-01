b1c45021367d42370f3569e23e7e0a264b3dbb28
rerroui <81562866+rerroui@users.noreply.github.com>
2022-09-12 13:54:09 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+//Задача 1
+
+//function radius($x, $y, $R){
+//    if(sqrt($x**2 + $y**2) <= $R){
+//        return 'Точка принадлежит кругу';
+//    }
+//    else{
+//        return 'Точка не принадлежит кругу';
+//    }
+//};
+//echo radius(1, -1, 3);
+
+//Задача 2
+
+//function thousand($user_count){
+//    if($user_count%1000==0){
+//        return 'Вы счастливый обладатель скидки XD';
+//    }
+//    else{
+//        return 'Вы будете без скидки :(';
+//    }
+//};
+//echo thousand(23000);
+
+//Задача 3
+
+//function zero($number){
+//    if($number === 0){
+//        return 'Число равно нулю';
+//    }
+//    elseif ($number > 0){
+//        return 'Число положительное';
+//    }
+//    elseif ($number < 0){
+//        return 'Число отрицательное';
+//    }
+//};
+//echo zero(-124);
+
+//Задача 4 (Четное)
+
+//function zero_one($number){
+//    if($number%2==0){
+//        return '1';
+//    }
+//    else{
+//        return '0';
+//    }
+//}
+//echo zero_one(12);
+
+//Задача Ежедневник
+
+function diary(){
+
+}
