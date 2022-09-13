844888c6791f01453c21c463ee78ea1f39fd53ea
rerroui <81562866+rerroui@users.noreply.github.com>
2022-09-13 14:51:15 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -62,40 +62,175 @@
 
     //Задача 5
 
-    function how_hour($hour){
-        $last_number = $hour%10;
-        if($last_number === 1){
-            return ($hour.' час');
-        }
-        elseif ($last_number>1 && $last_number<5){
-            return ($hour.' часa');
-        }
-        else{
-            return ($hour.' часов');
-        }
-    }
-    echo how_hour(4);
+//    function how_hour($hour){
+//        $last_number = $hour%10;
+//        if($last_number === 1){
+//            return ($hour.' час');
+//        }
+//        elseif ($last_number>1 && $last_number<5){
+//            return ($hour.' часa');
+//        }
+//        else{
+//            return ($hour.' часов');
+//        }
+//    }
+//    echo how_hour(4);
+//
+//    ?>
 
-    ?>
+    <h1>Артикул</h1>
 
     <?php
 
-//Задача Ежедневник
+    //Задача Артикул
 
-// function diary($h_start_1, $m_start_1, $s_start_1, $h_finish_1, $m_finish_1, $s_finish_1, $h_start_2, $m_start_2, $s_start_2, $h_finish_2, $m_finish_2, $s_finish_2){
-//    $number_one = $h_start_1.$m_start_1.$s_start_1;
-//    $number_two = $h_finish_1.$m_finish_1.$s_finish_1;
-//    $number_three =  $h_start_2.$m_start_2.$s_start_2;
-//    $number_four = $h_finish_2.$m_finish_2.$s_finish_2;
+    $number_art = 3454;
+    printf('%06d', $number_art)
 
-//    if($number_one>$number_three){
-//        if($number_two>$number_four){
-//
-//        }
-//    }
-    // }
-// echo diary(14,10,30,15,00,20,14,50,13,15,40,10);
+    ?>
+
+    <h1>Время в формате hh:mm:ss</h1>
+
+    <?php
+
+    //Задача Часы
+
+    $second = 20490;
+    $hours = floor($second / 3600);
+    $minutes = floor(($second / 60) % 60);
+    $seconds = $second % 60;
+    printf('%02d:%02d:%02d', $hours, $minutes, $seconds)
 ?>
+
+    <h1>ФИО</h1>
+
+    <?php
+
+    //Задача ФИО
+
+    $first_name = 'Наталья ';
+    $second_name = 'Жуйкова ';
+    $patronymic = 'Евгеньевна ';
+    $FIO = $second_name.$first_name.$patronymic;
+    echo $FIO, mb_substr($second_name, 0, 1),mb_substr($first_name, 0, 1),mb_substr($patronymic, 0, 1);
+
+    ?>
+
+    <h1>Короткий пароль</h1>
+
+    <?php
+
+    //Задача Короткий пароль
+
+   $password = 1234567;
+   if(mb_strlen(($password)) < 8){
+       echo 'Слишком короткий пароль!';
+   }
+   else{
+       echo 'Хороший пароль ;)';
+   }
+
+    ?>
+
+    <h1>Пробел</h1>
+
+    <?php
+
+    //Задача Пробел
+
+    $password_2 = 'gh b1234';
+    if(mb_substr_count($password_2, ' ')>0){
+        echo 'Внимание! Пароль не должен содержать пробелов!';
+    }
+    else{
+        echo 'Хороший пароль! ;)';
+    }
+
+    ?>
+
+    <h1>Ровно</h1>
+
+    <?php
+
+    //Задача Ровно
+
+    $str_one = 'Привет';
+    $str_two = 'Привет';
+    if(mb_strlen($str_one) === mb_strlen($str_two)){
+        echo 'Количество букв совпадает!';
+    }
+    else{
+        echo 'Количество букв НЕ совпадает!';
+    }
+
+    ?>
+
+    <h1>Подробнее</h1>
+
+    <?php
+
+    //Задача Подробнее
+
+    $text = 'К северо-востоку от Москвы неподалеку от Воря-Богородского лесничества расположено озеро Черное. Окруженное тенью и прохладой хвойных деревьев.';
+    if(mb_strlen($text) > 100){
+        echo mb_substr($text, 0, 50)." <a href='#'>Подробнее</a>";
+    }
+    else{
+        echo $text;
+    }
+    ?>
+
+    <h1>Короче</h1>
+
+    <?php
+
+    //Задача Короче
+
+    $word = 'Обороноспособность';
+    if(mb_strlen($word) > 7){
+        echo mb_substr($word, 0, 4).'-'.mb_substr($word, -2, 2);
+    }
+    else{
+        echo $word;
+    }
+
+    ?>
+
+    <h1>Коровы</h1>
+
+    <?php
+
+    //Задача Коровы
+
+    $str = 'самостоятельность';
+    $sign = 'с';
+    echo "В строке '$str' символ '$sign' встречается ".mb_substr_count($str, $sign)." раза.";
+
+    ?>
+
+    <h1>Caps Lock</h1>
+
+    <?php
+
+    //Задача Caps Lock
+
+    $password_qwerty = 'QWERTY123';
+
+
+    ?>
+
+    <h1>Регистронезависимые коровы</h1>
+
+    <?php
+
+    //Задача Регистронезависимые коровы
+
+    $str_2 = 'САМОСТОЯТЕЛЬНОСТЬ';
+    $sign_2 = 'с';
+    echo "В строке '$str_2' символ '$sign_2' встречается ".mb_substr_count(mb_convert_case($str_2, MB_CASE_UPPER), mb_convert_case($sign_2, MB_CASE_UPPER))." раза.";
+
+    ?>
+
 
 </body>
 </html>
