329aa3c93fdbf8587604c28a55ac2e5273c5e55f
rerroui <81562866+rerroui@users.noreply.github.com>
2022-10-17 12:28:29 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -5,232 +5,201 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Жуйкова 42В (Занятие 3)</title>
 </head>
 <body>
-    <?php
-//Задача 1
+<h1>Артикул</h1>
 
-//function radius($x, $y, $R){
-//    if(sqrt($x**2 + $y**2) <= $R){
-//        return 'Точка принадлежит кругу';
-//    }
-//    else{
-//        return 'Точка не принадлежит кругу';
-//    }
-//};
-//echo radius(1, -1, 3);
+<?php
 
-//Задача 2
+//Задача Артикул
 
-//function thousand($user_count){
-//    if($user_count%1000==0){
-//        return 'Вы счастливый обладатель скидки XD';
-//    }
-//    else{
-//        return 'Вы будете без скидки :(';
-//    }
-//};
-//echo thousand(23000);
+$number_art = 3454;
+printf('%06d', $number_art)
 
-//Задача 3
-
-//function zero($number){
-//    if($number === 0){
-//        return 'Число равно нулю';
-//    }
-//    elseif ($number > 0){
-//        return 'Число положительное';
-//    }
-//    elseif ($number < 0){
-//        return 'Число отрицательное';
-//    }
-//};
-//echo zero(-124);
-
-//Задача 4 (Четное)
-
-//function zero_one($number){
-//    if($number%2==0){
-//        return '1';
-//    }
-//    else{
-//        return '0';
-//    }
-//}
-//echo zero_one(12);
-
-    //Задача 5
-
-//    function how_hour($hour){
-//        $last_number = $hour%10;
-//        if($last_number === 1){
-//            return ($hour.' час');
-//        }
-//        elseif ($last_number>1 && $last_number<5){
-//            return ($hour.' часa');
-//        }
-//        else{
-//            return ($hour.' часов');
-//        }
-//    }
-//    echo how_hour(4);
-//
-//    ?>
-
-    <h1>Артикул</h1>
-
-    <?php
-
-    //Задача Артикул
-
-    $number_art = 3454;
-    printf('%06d', $number_art)
-
-    ?>
-
-    <h1>Время в формате hh:mm:ss</h1>
-
-    <?php
-
-    //Задача Часы
-
-    $second = 20490;
-    $hours = floor($second / 3600);
-    $minutes = floor(($second / 60) % 60);
-    $seconds = $second % 60;
-    printf('%02d:%02d:%02d', $hours, $minutes, $seconds)
 ?>
 
-    <h1>ФИО</h1>
+<h1>Время в формате hh:mm:ss</h1>
 
-    <?php
+<?php
 
-    //Задача ФИО
+//Задача Часы
 
-    $first_name = 'Наталья ';
-    $second_name = 'Жуйкова ';
-    $patronymic = 'Евгеньевна ';
-    $FIO = $second_name.$first_name.$patronymic;
-    echo $FIO, mb_substr($second_name, 0, 1),mb_substr($first_name, 0, 1),mb_substr($patronymic, 0, 1);
+$second = 20490;
+$hours = floor($second / 3600);
+$minutes = floor(($second / 60) % 60);
+$seconds = $second % 60;
+printf('%02d:%02d:%02d', $hours, $minutes, $seconds);
+echo '</br>'.'Если не обращать внимания на ошибку выше, то ответ выводится правильный. Не знаю как исправить эту error (до этого её не было) :(('
+?>
 
-    ?>
+<h1>ФИО</h1>
 
-    <h1>Короткий пароль</h1>
+<?php
 
-    <?php
+//Задача ФИО
 
-    //Задача Короткий пароль
+$first_name = 'Наталья ';
+$second_name = 'Жуйкова ';
+$patronymic = 'Евгеньевна ';
+$FIO = $second_name.$first_name.$patronymic;
+echo $FIO, mb_substr($second_name, 0, 1),mb_substr($first_name, 0, 1),mb_substr($patronymic, 0, 1);
 
-   $password = 1234567;
-   if(mb_strlen(($password)) < 8){
-       echo 'Слишком короткий пароль!';
-   }
-   else{
-       echo 'Хороший пароль ;)';
-   }
+?>
 
-    ?>
+<h1>Короткий пароль</h1>
 
-    <h1>Пробел</h1>
+<?php
 
-    <?php
+//Задача Короткий пароль
 
-    //Задача Пробел
+$password = 1234567;
+if(mb_strlen(($password)) < 8){
+    echo 'Слишком короткий пароль!';
+}
+else{
+    echo 'Хороший пароль ;)';
+}
 
-    $password_2 = 'gh b1234';
-    if(mb_substr_count($password_2, ' ')>0){
-        echo 'Внимание! Пароль не должен содержать пробелов!';
-    }
-    else{
-        echo 'Хороший пароль! ;)';
-    }
+?>
 
-    ?>
+<h1>Пробел</h1>
 
-    <h1>Ровно</h1>
+<?php
 
-    <?php
+//Задача Пробел
 
-    //Задача Ровно
+$password_2 = 'gh b1234';
+if(mb_substr_count($password_2, ' ')>0){
+    echo 'Внимание! Пароль не должен содержать пробелов!';
+}
+else{
+    echo 'Хороший пароль! ;)';
+}
 
-    $str_one = 'Привет';
-    $str_two = 'Привет';
-    if(mb_strlen($str_one) === mb_strlen($str_two)){
-        echo 'Количество букв совпадает!';
-    }
-    else{
-        echo 'Количество букв НЕ совпадает!';
-    }
+?>
 
-    ?>
+<h1>Ровно</h1>
 
-    <h1>Подробнее</h1>
+<?php
 
-    <?php
+//Задача Ровно
 
-    //Задача Подробнее
+$str_one = 'Привет';
+$str_two = 'Привет';
+if(mb_strlen($str_one) === mb_strlen($str_two)){
+    echo 'Количество букв совпадает!';
+}
+else{
+    echo 'Количество букв НЕ совпадает!';
+}
 
-    $text = 'К северо-востоку от Москвы неподалеку от Воря-Богородского лесничества расположено озеро Черное. Окруженное тенью и прохладой хвойных деревьев.';
-    if(mb_strlen($text) > 100){
-        echo mb_substr($text, 0, 50)." <a href='#'>Подробнее</a>";
-    }
-    else{
-        echo $text;
-    }
-    ?>
+?>
 
-    <h1>Короче</h1>
+<h1>Подробнее</h1>
 
-    <?php
+<?php
 
-    //Задача Короче
+//Задача Подробнее
 
-    $word = 'Обороноспособность';
-    if(mb_strlen($word) > 7){
-        echo mb_substr($word, 0, 4).'-'.mb_substr($word, -2, 2);
-    }
-    else{
-        echo $word;
-    }
+$text = 'К северо-востоку от Москвы неподалеку от Воря-Богородского лесничества расположено озеро Черное. Окруженное тенью и прохладой хвойных деревьев.';
+if(mb_strlen($text) > 100){
+    echo mb_substr($text, 0, 50)." <a href='#'>Подробнее</a>";
+}
+else{
+    echo $text;
+}
+?>
 
-    ?>
+<h1>Короче</h1>
 
-    <h1>Коровы</h1>
+<?php
 
-    <?php
+//Задача Короче
 
-    //Задача Коровы
+$word = 'Обороноспособность';
+if(mb_strlen($word) > 7){
+    echo mb_substr($word, 0, 4).'-'.mb_substr($word, -2, 2);
+}
+else{
+    echo $word;
+}
 
-    $str = 'самостоятельность';
-    $sign = 'с';
-    echo "В строке '$str' символ '$sign' встречается ".mb_substr_count($str, $sign)." раза.";
+?>
 
-    ?>
+<h1>Коровы</h1>
 
-    <h1>Caps Lock</h1>
+<?php
 
-    <?php
+//Задача Коровы
 
-    //Задача Caps Lock
+$str = 'самостоятельность';
+$sign = 'с';
+echo "В строке '$str' символ '$sign' встречается ".mb_substr_count($str, $sign)." раза.";
 
+?>
+
+<h1>Caps Lock</h1>
+
+<?php
+//Задача Caps Lock
+function Password($str_pass){
     $password_qwerty = 'QWERTY123';
+    if($str_pass==='qwerty123'){
+        echo 'Добро пожаловать!';
+    }
+    elseif (mb_substr_count($str_pass, $password_qwerty)===1){
+        echo 'Возможно нажата клавиша Caps Lock!';
+    }
+    else{
+        echo 'Ошибка';
+    }
+}
+Password('QWERTY123');
+?>
 
+<h1>Регистронезависимые коровы</h1>
 
-    ?>
+<?php
 
-    <h1>Регистронезависимые коровы</h1>
+//Задача Регистронезависимые коровы
 
-    <?php
+$str_2 = 'САМОСТОЯТЕЛЬНОСТЬ';
+$sign_2 = 'с';
+echo "В строке '$str_2' символ '$sign_2' встречается ".mb_substr_count(mb_convert_case($str_2, MB_CASE_UPPER), mb_convert_case($sign_2, MB_CASE_UPPER))." раза.";
 
-    //Задача Регистронезависимые коровы
+?>
 
-    $str_2 = 'САМОСТОЯТЕЛЬНОСТЬ';
-    $sign_2 = 'с';
-    echo "В строке '$str_2' символ '$sign_2' встречается ".mb_substr_count(mb_convert_case($str_2, MB_CASE_UPPER), mb_convert_case($sign_2, MB_CASE_UPPER))." раза.";
+<h1>Курсив</h1>
+<?php
+function italic($text, $word_italic){
+    echo $text."<br>";
+    $word_italic=mb_convert_case($word_italic, MB_CASE_UPPER);
+    $text=str_replace($word_italic, "<i>$word_italic</i>", $text);
+    $word_italic=mb_convert_case($word_italic, MB_CASE_TITLE);
+    $text=str_replace($word_italic, "<i>$word_italic</i>", $text);
+    $word_italic=mb_convert_case($word_italic, MB_CASE_LOWER);
+    $text=str_replace($word_italic, "<i>$word_italic</i>", $text);
 
-    ?>
+    echo $text;
+}
+italic('Этого кота зовут Персик, этого КОТА зовут Барсик, а этого Кота зовут Валера.', 'кота');
+?>
 
+<h1>Тег</h1>
+
+<?php
+
+//Тег
+
+function tag($str_lorem){
+    $index_1=mb_stripos($str_lorem,'>');
+    $index_2=mb_stripos($str_lorem,'<',-1);
+    $length=mb_strlen($str_lorem)-$index_2+$index_1;
+    echo mb_substr($str_lorem,$index_1+1,$length);
+}
+tag("<p>Lorem ipsum dolor.</p>");
+?>
 
 </body>
 </html>
